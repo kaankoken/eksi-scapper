255b50521e84981e21350d6d216f5d267f35a091
kaankoken <kaan_taha_koken@hotmail.com>
2021-10-20 16:24:45 +0300

    Fix http 502 error on async request handler fixation via delay

--- http_requests.py
+++ http_requests.py
@@ -1,31 +1,29 @@
+from asyncio.tasks import sleep
 from typing import Any, Coroutine, List, Tuple
 from aiohttp.client import ClientSession
 import asyncio
 import aiohttp
+from requests.api import get
 
-from helper import random_headers
 
-
-async def fetch(url, session: ClientSession) -> Coroutine[Any, Any, bytes]:
-    """
-    asynchronous get request
-    """
-    async with session.get(url) as response:
+async def fetch(site) -> Coroutine[Any, Any, bytes]:
+    async with aiohttp.ClientSession() as session, \
+            session.get(site) as response:
         return await response.read()
 
 
-async def fetch_many(loop, urls) -> Coroutine[Any, Any, Tuple]:
-    '''
-        many asynchronous get requests, gathered
-    '''
-    async with aiohttp.ClientSession(headers=random_headers()) as session:
-        tasks = [loop.create_task(fetch(url, session)) for url in urls]
-        return await asyncio.gather(*tasks)
+async def get_all_req(loop, urls: List[str]) -> List:
+    tasks = []
+    for url in urls:
+        await sleep(0.3)
+        task = loop.create_task(fetch(url))
+        tasks.append(task)
+    return await asyncio.gather(*tasks)
 
 
-def async_aiohttp_get_all(urls) -> List:
+def async_aiohttp_get_all(urls: List[str]) -> List:
     '''
         performs asynchronous get requests
     '''
     loop = asyncio.get_event_loop()
-    return loop.run_until_complete(fetch_many(loop, urls))
+    return loop.run_until_complete(get_all_req(loop, urls))
