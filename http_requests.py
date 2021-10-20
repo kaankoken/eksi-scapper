118279fb6507263043b92dec70bb8f43234be6c4
kaankoken <kaan_taha_koken@hotmail.com>
2021-10-20 10:29:58 +0300

    Add async request (like promise all) methods added to http request file

---
+++ http_requests.py
@@ -0,0 +1,31 @@
+from typing import Any, Coroutine, Tuple
+from aiohttp.client import ClientSession
+import asyncio
+import aiohttp
+
+from helper import random_headers
+
+
+async def fetch(url, session: ClientSession) -> Coroutine[Any, Any, bytes]:
+    """
+    asynchronous get request
+    """
+    async with session.get(url) as response:
+        return await response.read()
+
+
+async def fetch_many(loop, urls) -> Coroutine[Any, Any, Tuple]:
+    '''
+        many asynchronous get requests, gathered
+    '''
+    async with aiohttp.ClientSession(headers=random_headers()) as session:
+        tasks = [loop.create_task(fetch(url, session)) for url in urls]
+        return await asyncio.gather(*tasks)
+
+
+def async_aiohttp_get_all(urls) -> Tuple:
+    '''
+        performs asynchronous get requests
+    '''
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(fetch_many(loop, urls))
